64431db8068943a4d8789a728e1b960398fe920c
Busra Mestan <bmestan50@gmail.com>
2025-05-12 23:13:34 +0300

    create calculate

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,44 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Birinci sayıyı girin: ");
+        double sayi1 = scanner.nextDouble();
+
+        System.out.println("İkinci sayıyı girin: ");
+        double sayi2 = scanner.nextDouble();
+
+        System.out.println("Yapmak istediğiniz işlemi seçin (+, -, *, /): ");
+        char islem = scanner.next().charAt(0);
+
+        double sonuc;
+        switch (islem) {
+            case '+':
+                sonuc = sayi1 + sayi2;
+                break;
+            case '-':
+                sonuc = sayi1 - sayi2;
+                break;
+            case '*':
+                sonuc = sayi1 * sayi2;
+                break;
+            case '/':
+                if (sayi2 != 0) {
+                    sonuc = sayi1 / sayi2;
+                } else {
+                    System.out.println("Hata: Bir sayı sıfıra bölünemez.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Geçersiz işlem seçimi.");
+                return;
+        }
+
+        System.out.println("Sonuç: " + sonuc);
     }
 }
